91deabb11ce64310c6ba24532c79548bbf590499
ridafd <ridafakherlden@gmail.com>
2020-08-23 22:46:31 +0300

    Section2 14.Introduction to layout files

--- resources/views/todos/index.blade.php
+++ resources/views/todos/index.blade.php
@@ -1,18 +1,10 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+@extends('layouts.app')
 
-    <!-- bootstrap -->
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" crossorigin="anonymous">
+@section('title')
+    abo roud
+@endsection
 
-    <title>Document</title>
-</head>
-<body>
-
-    <div class="container">
+@section('content')
         <h1 class="text-center">Todos page</h1>
         <div class="row justify-content-center">
             <div class="col-md-8">
@@ -34,9 +26,4 @@
             </div>
         </div>
     </div>
-                        
-                 
-    
-
-</body>
-</html>
+@endsection
--- resources/views/todos/show.blade.php
+++ resources/views/todos/show.blade.php
@@ -1,38 +1,30 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+@extends('layouts.app')
 
-    <!-- bootstrap -->
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" crossorigin="anonymous">
+@section('title')
+    {{ $todo->name }}
+@endsection
 
-    <title>Document</title>
-</head>
-<body>
-    
-    <div class="container">
 
-        <div class="text-center my-5">
-            {{ $todo->name }}
-        </div>
+@section('content')
 
-        <div class="row justify-content-center">
-            <div class="col-md-6">
-                <div class="card card-def">
-                    <div class="card-header">
-                        Details
-                    </div>
-                </div>
-        
-                <div class="card-body">
-                    {{ $todo->description }}
-                </div>
-            </div>
-        </div>
-
+<div class="text-center my-5">
+        {{ $todo->name }} 
     </div>
 
-</body>
-</html>
+    <div class="row justify-content-center">
+        <div class="col-md-6">
+            <div class="card card-def">
+                <div class="card-header">
+                    Details
+                </div>
+            </div>
+    
+            <div class="card-body">
+                {{ $todo->description }}
+            </div>
+        </div>
+    </div>
+
+
+@endsection
+    
