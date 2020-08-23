f803d5d99d87c73248bbd9456e27ae1abe100b7e
ridafd <ridafakherlden@gmail.com>
2020-08-23 16:57:17 +0300

    Section2 12.Styling view with bootstrap

--- resources/views/todos/index.blade.php
+++ resources/views/todos/index.blade.php
@@ -10,15 +10,28 @@
 </head>
 <body>
 
-        <h1>Todos page</h1>
-        
-        <div>Todos</div>
-            
-        @foreach($todos as $todo)
-        <li>
-            {{ $todo->name }}
-        </li>
-        @endforeach()
+    <div class="container">
+        <h1 class="text-center">Todos page</h1>
+        <div class="row justify-content-center">
+            <div class="col-md-8">
+                <div class="card card-default">
+                    <div class="card-header">
+                        Todos
+                    </div>
+                    <div class="card-body">
+                        <ul class="list-group">
+                            @foreach($todos as $todo)
+                            <li class="list-group-item">
+                                {{ $todo->name }}
+                                <button class="btn btn-primary float-right">view</button>
+                            </li>
+                            @endforeach()
+                        </ul>
+                    </div>
+                </div> 
+            </div>
+        </div>
+    </div>
                         
                  
     
